b3b9fb36945db515df55d3fa634eb7fe0234ed7b
Vitalii <vetal.shtager@gmail.com>
2022-04-05 20:50:16 -0400

    commit tree 367254fd58a6652d8de221233aab7f0984d001fe

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  
+}
+saturdayFun()
+
+const mondayWork = function(event = "go to the office") {
+    return `This Monday, I will ${event}.`
+}
+mondayWork
+
+
+// function wrapAdjective( name = "a hard worker" ) {
+//     const part1 = "You are"
+//     return function() {
+//         return `${part1} *${name}*!`
+//     }
+// }
+// wrapAdjective("||a dedicated programmer")()
+
+
+function wrapAdjective(star = "*") {
+    const part1 = "You are"
+    return function(name ="a hard worker") {
+        return `${part1} ${star}${name}${star}!`
+    }
+        
+}
+wrapAdjective("||")("a dedicated programmer");
